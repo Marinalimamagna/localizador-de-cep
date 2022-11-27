b23530b2c8049823e80441db41d26370841feeae
Marina Magna de Lima <108198872+Marinalimamagna@users.noreply.github.com>
2022-11-27 09:36:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {FiSearch} from 'react-icons/fi'
+import './styles.css';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep"
+        />
+        <button className="buttonSearch" >
+          <FiSearch size={20} color="#fff"/>
+        </button>
+      </div>
+      <main className='main'>
+        <h2>CEP: 35845000</h2>
+
+        <span>Rua santa luzia</span>
+        <span>linha verde</span>
+        <span>Centro</span>
+        <span>Santana do riacho-Mg</span>
+
+      </main>
     </div>
-  );
-}
-
-export default App;
+  }
+export default App
